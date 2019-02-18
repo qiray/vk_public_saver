e4509023f1f51f7306ac9b3eea6441b6d6f3dab3
qiray <zotovyaa@mail.ru>
2019-02-18 14:34:00 +0300

    User/pass input added

--- main.go
+++ main.go
@@ -1,18 +1,49 @@
 package main
 
 import (
+	"bufio"
+	"flag"
 	"fmt"
 	"os"
+	"strings"
+	"syscall"
+
+	"golang.org/x/crypto/ssh/terminal"
 )
 
+func setCredentials(settings *AppSettings) {
+	reader := bufio.NewReader(os.Stdin)
+
+	fmt.Print("Enter Username: ")
+	username, _ := reader.ReadString('\n')
+
+	fmt.Print("Enter Password: ")
+	bytePassword, err := terminal.ReadPassword(int(syscall.Stdin))
+	if err != nil {
+		fmt.Println("Failed to get password")
+		return
+	}
+	password := string(bytePassword)
+	settings.userdata = make(map[string]string)
+	settings.userdata["email"] = strings.TrimSpace(username)
+	settings.userdata["pass"] = strings.TrimSpace(password)
+}
+
 func main() {
+	userdataFlag := flag.Bool("userdata", false, "Use userdata.json for email and pass")
+	flag.Parse()
 	settings, err := loadSettings("settings.json")
 	if err != nil {
 		fmt.Println("Settings load failed. Closing...")
 		os.Exit(1)
 	}
-	userdata, _ := loadJSONFileMap("userdata.json")
-	settings.userdata = userdata //TODO: maybe read from input
+	if *userdataFlag {
+		userdata, _ := loadJSONFileMap("userdata.json")
+		settings.userdata = userdata
+	} else {
+		setCredentials(&settings)
+	}
+
 	login(&settings)
 	print(settings.token, "\n")
 	wallGet(settings)
