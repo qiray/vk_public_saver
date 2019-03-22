8186fcaf747335d92252f68fc0224de0b945fcd0
qiray <zotovyaa@mail.ru>
2019-03-23 00:38:18 +0300

    License info added to files

--- types.go
+++ types.go
@@ -1,3 +1,22 @@
+// vk_public_saver - tool for saving walls data from vk.com
+// Copyright (c) 2019, Yaroslav Zotov, https://github.com/qiray/
+// All rights reserved.
+
+// This file is part of vk_public_saver.
+
+// vk_public_saver is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+
+// vk_public_saver is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+
+// You should have received a copy of the GNU General Public License
+// along with vk_public_saver.  If not, see <https://www.gnu.org/licenses/>.
+
 package main
 
 //Read https://vk.com/dev/attachments_w for more info about attachments
